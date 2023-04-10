e2e8afb922b8a3b71c95100d8813a84a2a179f8f
Danack <Danack@basereality.com>
2023-04-10 12:01:26 +0100

    Some sort of benchmark.

---
+++ generate_benchmark_code.php
@@ -0,0 +1,91 @@
+<?php
+
+
+function get_namespace_code(string $namespace)
+{
+    $code = <<<  PHP
+
+
+namespace $namespace {
+    function testing() {
+        echo "strlen is " . strlen("foobar") . "\n";
+    }
+}
+
+PHP;
+
+    return $code;
+}
+
+$contents = "<?php";
+
+$autoload_code = <<< 'PHP'
+namespace {
+
+ echo "Hello world\n";
+
+function my_function_loader(string $function_name) {
+//    echo "Autoloader called with $function_name \n";
+    if (str_starts_with($function_name, 'App') === false) {
+        return;
+    }
+}
+
+if (rand(0,1) == 0) {
+    // actually setup function autoloader
+    echo "autoloarder funct\n";
+    autoload_register_function(my_function_loader(...));
+} 
+else {
+   // deliberately wrong, but similar, to make equivalent
+   // number of operations in setup.
+   echo "autoloarder class\n";
+   autoload_register_class(my_function_loader(...));
+}
+
+}
+
+PHP;
+
+$contents .= "\n\n";
+
+$contents .= $autoload_code;
+
+for ($i=0; $i < 10; $i += 1) {
+    $contents .= get_namespace_code("test_$i");
+}
+
+
+$contents .= "\n\n";
+$contents .= "\n\n";
+
+$contents .= "namespace {\n";
+
+
+// Not sure whether increasing this makes the overhead smaller
+// or bigger.
+$number_of_namespaces = 10;
+
+// The function lookup happens once, so making this number
+// higher makes the overhead be relatively smaller.
+$invocations_per_namespace = 10;
+
+for ($j=0; $j < $invocations_per_namespace; $j += 1) {
+    for ($i = 0; $i < $number_of_namespaces; $i += 1) {
+        $namespace = "test_$i";
+        $contents .= "\\$namespace\\testing();\n";
+        $contents .= "\n";
+    }
+}
+
+$contents .= "}\n";
+
+file_put_contents(__DIR__ . "/benchmark.php", $contents);
+
+
+
+
+
+
+
+
